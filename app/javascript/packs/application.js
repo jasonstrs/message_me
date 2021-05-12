97339f25ec41d7e33385c6a3cc05850b4cd3e616
jasonstrs <jason.sautieres@gmail.com>
2021-05-12 15:25:41 +0200

    add favicon and navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("semantic-ui-sass")
+
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
